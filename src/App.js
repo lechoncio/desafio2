e29ad8e5873733edbb0bccd2bad6e636d90bd257
Rafael Ketelhohn <rafaelk@gmail.com>
2022-08-17 12:18:44 -0300

    desafio2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (    
+    <>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="./index.html"><img class="logo" src="../images/logo.png" alt="logo"></img></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mx-auto">
+              <li class="nav-item">
+                <a class="nav1" href="./armados.html"><b>SkateBoards Armados</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav1" href="./tablas.html"><b>Tablas</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav1" aria-current="page" href="./long.html"><b>LongBoards</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav1" aria-current="page" href="./accesorios.html"><b>Accesorios</b></a>
+              </li>
+            </ul>
+            
+          </div>
+          <ul class="navbar-nav mx-auto">
+              <li class="nav-item">
+                <a class="nav1" href="./armados.html"><b>Login</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav1" href="./tablas.html"><b>Registrarse</b></a>
+              </li>
+              </ul>
+          
+      </div>
+    </nav>       
+    </>
   );
 }
 
